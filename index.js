0106b936a19fdb7b0755614fb980b08f07071d41
Eric Peterson <ericpeterson91@live.ca>
2022-09-12 21:56:51 -0600

    todo additions work

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const list_el = document.querySelector('#tasks')
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     const task = input.value;
-    console.log(task)
+
+    if(!task) {
+        alert('Please fill out a task')
+        return
+    }
+
+    const task_el = document.createElement('div')
+    task_el.classList.add('task')
+
+    const task_content_el = document.createElement('div')
+    task_content_el.classList.add('content')
+    task_content_el.innerText = task
+
+    task_el.appendChild(task_content_el)
+    list_el.appendChild(task_el)
+    
 
 })
